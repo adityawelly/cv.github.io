5dfe5dc28815a5eccb2152ac5725c00a072c536f
wildannajahw <wildan.najah@gmail.com>
2018-10-15 23:25:25 +0700

    first commit

---
+++ cv.github.io/index.php
@@ -0,0 +1,211 @@
+<?php
+    session_start();
+    include('config.php');
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Electronic Store a Ecommerce Online Shopping Category Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Electronic Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free web designs for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+	function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/fasthover.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<link rel="stylesheet" href="css/jquery.countdown.css" /> <!-- countdown --> 
+<!-- //js -->  
+<!-- web fonts --> 
+<link href='//fonts.googleapis.com/css?family=Glegoo:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- //web fonts -->  
+<!-- start-smooth-scrolling -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- //end-smooth-scrolling --> 
+</head> 
+<body>
+	<!-- for bootstrap working -->
+	<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+	<!-- //for bootstrap working -->
+	<!-- header -->
+	<?php include 'header1.php'?>
+    <!-- special-deals -->
+	<div class="special-deals">
+		<div class="container">
+			<h2>Special Deals</h2>
+			<div class="w3agile_special_deals_grids">
+				<div class="col-md-7 w3agile_special_deals_grid_left">
+					<div class="w3agile_special_deals_grid_left_grid">
+						<img src="images/21.jpg" alt=" " class="img-responsive" />
+						<div class="w3agile_special_deals_grid_left_grid_pos1">
+							<h5>30%<span>Off/-</span></h5>
+						</div>
+						<div class="w3agile_special_deals_grid_left_grid_pos">
+							<h4>We Offer <span>Best Products</span></h4>
+						</div>
+					</div>
+					<div class="wmuSlider example1">
+						<div class="wmuSliderWrapper">
+							<article style="position: absolute; width: 100%; opacity: 0;"> 
+								<div class="banner-wrap">
+									<div class="w3agile_special_deals_grid_left_grid1">
+										<img src="images/t1.png" alt=" " class="img-responsive" />
+										<p>Quis autem vel eum iure reprehenderit qui in ea voluptate 
+											velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+											eum fugiat quo voluptas nulla pariatur</p>
+										<h4>Laura</h4>
+									</div>
+								</div>
+							</article>
+							<article style="position: absolute; width: 100%; opacity: 0;"> 
+								<div class="banner-wrap">
+									<div class="w3agile_special_deals_grid_left_grid1">
+										<img src="images/t2.png" alt=" " class="img-responsive" />
+										<p>Quis autem vel eum iure reprehenderit qui in ea voluptate 
+											velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+											eum fugiat quo voluptas nulla pariatur</p>
+										<h4>Michael</h4>
+									</div>
+								</div>
+							</article>
+							<article style="position: absolute; width: 100%; opacity: 0;"> 
+								<div class="banner-wrap">
+									<div class="w3agile_special_deals_grid_left_grid1">
+										<img src="images/t3.png" alt=" " class="img-responsive" />
+										<p>Quis autem vel eum iure reprehenderit qui in ea voluptate 
+											velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+											eum fugiat quo voluptas nulla pariatur</p>
+										<h4>Rosy</h4>
+									</div>
+								</div>
+							</article>
+						</div>
+					</div>
+						<script src="js/jquery.wmuSlider.js"></script> 
+						<script>
+							$('.example1').wmuSlider();         
+						</script> 
+				</div>
+				<div class="col-md-5 w3agile_special_deals_grid_right">
+					<img src="images/20.jpg" alt=" " class="img-responsive" />
+					<div class="w3agile_special_deals_grid_right_pos">
+						<h4>Women's <span>Special</span></h4>
+						<h5>save up <span>to</span> 30%</h5>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!-- //special-deals -->
+	<!-- new-products -->
+	<div class="new-products">
+		<div class="container">
+			<h3>New Products</h3>
+			<div class="container">
+
+        <?php include 'product-latest.php'; ?>
+        
+        
+			</div>
+		</div>
+	</div>
+	<!-- //new-products -->
+	<!-- top-brands -->
+	<div class="top-brands">
+		<div class="container">
+			<h3>Top Brands</h3>
+			<div class="sliderfig">
+				<ul id="flexiselDemo1">			
+					<li>
+						<img src="images/tb1.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/tb2.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/tb3.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/tb4.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/tb5.jpg" alt=" " class="img-responsive" />
+					</li>
+				</ul>
+			</div>
+			<script type="text/javascript">
+					$(window).load(function() {
+						$("#flexiselDemo1").flexisel({
+							visibleItems: 4,
+							animationSpeed: 1000,
+							autoPlay: true,
+							autoPlaySpeed: 3000,    		
+							pauseOnHover: true,
+							enableResponsiveBreakpoints: true,
+							responsiveBreakpoints: { 
+								portrait: { 
+									changePoint:480,
+									visibleItems: 1
+								}, 
+								landscape: { 
+									changePoint:640,
+									visibleItems:2
+								},
+								tablet: { 
+									changePoint:768,
+									visibleItems: 3
+								}
+							}
+						});
+						
+					});
+			</script>
+			<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+		</div>
+	</div>
+	<!-- //top-brands --> 
+	<!-- newsletter -->
+	<div class="newsletter">
+		<div class="container">
+			<div class="col-md-6 w3agile_newsletter_left">
+				<h3>Newsletter</h3>
+				<p>Excepteur sint occaecat cupidatat non proident, sunt.</p>
+			</div>
+			<div class="col-md-6 w3agile_newsletter_right">
+				<form action="#" method="post">
+					<input type="email" name="Email" placeholder="Email" required="">
+					<input type="submit" value="" />
+				</form>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!-- //newsletter -->
+	<?php include 'footer.php';?>
+	
+	
+
+</body>
+</html>
